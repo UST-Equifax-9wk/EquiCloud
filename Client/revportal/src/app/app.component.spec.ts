00830abf9a82f89a32cc61c096998877dacc4bae
BeepingJohnson <jdfranco@asu.edu>
2024-01-17 10:47:54 -0700

    Actual name change this time to match server

--- Client/revportal/src/app/app.component.spec.ts
+++ Client/revportal/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rev-portal' title`, () => {
+  it(`should have the 'revportal' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rev-portal');
+    expect(app.title).toEqual('revportal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rev-portal');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, revportal');
   });
 });
