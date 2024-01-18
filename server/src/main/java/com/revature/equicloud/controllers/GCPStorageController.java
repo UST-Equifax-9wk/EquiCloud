0a107697dd46e2bdf3ce9989cb13e74cbed72542
BeepingJohnson <jdfranco@asu.edu>
2024-01-18 08:55:28 -0700

    missing blob

--- server/src/main/java/com/revature/equicloud/controllers/GCPStorageController.java
+++ server/src/main/java/com/revature/equicloud/controllers/GCPStorageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.cloud.storage.Blob;
 import com.revature.equicloud.services.GCPStorageService;
 
 @RestController
