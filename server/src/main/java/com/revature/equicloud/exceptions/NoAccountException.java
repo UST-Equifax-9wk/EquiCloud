370c5ff263d3456e4afc0bd4fd0ecaf3d6b9152d
BeepingJohnson <jdfranco@asu.edu>
2024-01-24 11:08:48 -0700

    front only

--- server/src/main/java/com/revature/equicloud/exceptions/NoAccountException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.equicloud.exceptions;
-
-public class NoAccountException extends Exception{
-    public static String noAccount = "Specified Account Not Found";
-    public NoAccountException(String message){
-        super(message);
-    }
-}
